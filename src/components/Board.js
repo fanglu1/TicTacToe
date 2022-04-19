eea92f3322c90f7fd0772a0076e0e8d854088f54
fanglu1 <fanglu0805@gmail.com>
2022-04-19 11:48:14 -0400

    pages working?

--- src/components/Board.js
+++ src/components/Board.js
@@ -13,7 +13,7 @@ const style = {
 
 const Board = ({ squares, onClick }) => (
   <div style={style}>
-    {squares.map((square, i) => (
+      {squares.map((square, i) => (
       <Square key={i} value={square} onClick={() => onClick(i)} />
     ))}
   </div>
